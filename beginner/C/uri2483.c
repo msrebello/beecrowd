b076f9240e151a589dac8e4bf2f3c377d2b0a650
matheus rebello <matheussrb@outlook.com>
2024-04-05 13:40:18 -0300

    Feliz Nataaal!

---
+++ beginner/C/uri2483.c
@@ -0,0 +1,23 @@
+/*
+    @author: Matheus Rebello;
+    @date: 05/03/2024;
+    @name: Feliz Nataaal!;
+
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    int indice;
+    scanf("%d", &indice);
+
+    printf("Feliz nat");
+    
+    for (int i = 0; i < indice; i++) {
+        printf("a");
+    }
+
+    printf("l!\n");
+    
+    return 0;
+}
