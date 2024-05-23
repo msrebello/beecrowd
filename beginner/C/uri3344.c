1d3a62089dd59c564c697b0578ddd87f4a3b9e83
matheus rebello <matheussrb@outlook.com>
2024-05-23 10:31:49 -0300

    feat: add Brute

---
+++ beginner/C/uri3344.c
@@ -0,0 +1,16 @@
+/*
+    @author: Matheus Rebello;
+    @date: 23/05/2024;
+    @name: Brute;
+
+*/
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", 4);
+    return 0;
+}
